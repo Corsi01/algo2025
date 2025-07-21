ab426fbc4715f31b231bd045b4a2a58f9b58ec5d
Andrea Corsico <andre.corsico@gmail.com>
2025-07-21 18:30:28 +0200

    Update train_multisubject.py

--- models/best_model/train_multisubject.py
+++ models/best_model/train_multisubject.py
@@ -6,25 +6,22 @@ FIXED: Added direct full training without intermediate steps
 import os
 import torch
 import numpy as np
-from utilities import (
+from ..utilities import (
     align_features_and_fmri_samples_extended,
     train_multitask_model,
     train_final_model_on_all_data,
     evaluate_multitask_model,
     evaluate_multitask_model_by_network,
-    create_ensemble_from_saved_models,
     make_prediction,
-    get_network_parcels,
-    setup_model_configurations,
-    set_seed
+    get_network_parcels
 )
-from optimization_utils import (
+from ..optimization_utils import (
     optimize_network_hyperparams,
     save_optimization_results,
     load_optimization_results,
     show_optimization_results
 )
-from data_utils import load_stimulus_features, load_fmri, atlas_schaefer, compute_encoding_accuracy
+from data_utils import load_fmri, atlas_schaefer, compute_encoding_accuracy, set_seed
 
 
 root_data_dir = '../../data'
