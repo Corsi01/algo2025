21848437ec39420d9ba84c041ca43da6e42c8587
Andrea Corsico <andre.corsico@gmail.com>
2025-07-23 19:16:20 +0200

    Update 02_pca_new.py

--- feature_extraction/02_pca_new.py
+++ feature_extraction/02_pca_new.py
@@ -8,7 +8,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--modality', type=str, default='visual')
+parser.add_argument('--modality', type=str, default='language',
+                    choices=['visual', 'visual_videomae', 'language', 'audio',  'audio_emo', 'language_multilingual'],
+                    help='Type of features to extract')
 parser.add_argument('--train', type=int, default=1)
 parser.add_argument('--test', type=int, default=1)
 parser.add_argument('--project_dir', default='../data/', type=str)
@@ -41,58 +43,108 @@ if args.train == 1:
     base_dir = os.path.join(args.project_dir, 'results', 'stimulus_features',
         'raw')
 
-    # Friends
     friends_seasons = [1, 2, 3, 4, 5, 6]
-    for i in friends_seasons:
-        #stimuli_list.append(os.path.join(base_dir, 'friends', args.modality,
-        #    'friends_s'+str(i)+'_features_'+args.modality+'.h5'))
-        stimuli_list.append(os.path.join(base_dir, 'friends', args.modality,
-            'friends_s'+str(i)+'_features_language.h5'))
-    # Movie10
     movie10_movies = ['bourne', 'figures', 'life', 'wolf']
-    for i in movie10_movies:
-        #stimuli_list.append(os.path.join(base_dir, 'movie10', args.modality,
-         #   'movie10_'+i+'_features_'+args.modality+'.h5'))
-        stimuli_list.append(os.path.join(base_dir, 'movie10', args.modality,
-            'movie10_'+i+'_features_language.h5'))
 
-    # Load the stimulus features for the encoding train stimuli
-    movie_splits = []
-    chunks_per_movie = []
-    for i, stim_dir in tqdm(enumerate(stimuli_list)):
-        data = h5py.File(stim_dir, 'r')
-        for m, movie in enumerate(data.keys()):
-            if i == 0 and m == 0: # if first episode of first season
-                if args.modality == 'language_multi' or args.modality == 'visual' or args.modality == 'audio' or args.modality == 'emotion':
-                    #features = np.asarray(data[movie][args.modality])
-                    features = np.asarray(data[movie]['language'])
-                else:
-                    print('errror')
-            else:
-                if args.modality == 'language_multi' or args.modality == 'visual' or args.modality == 'audio' or args.modality == 'emotion':
-                    features = np.append(
-                        #features, np.asarray(data[movie][args.modality]), 0)
-                        features, np.asarray(data[movie]['language']), 0)
-                else:
-                    print('error')
+    if args.modality in ['audio', 'visual', 'language']:
+        for i in friends_seasons:
+            stimuli_list.append(os.path.join(base_dir, 'friends', args.modality,
+                'friends_s'+str(i)+'_features_'+args.modality+'.h5'))
+        for i in movie10_movies:
+            stimuli_list.append(os.path.join(base_dir, 'movie10', args.modality,
+                'movie10_'+i+'_features_'+args.modality+'.h5'))
+
+    elif args.modality == 'visual_videomae':
+        for i in friends_seasons:
+            stimuli_list.append(os.path.join(base_dir, 'friends', args.modality,
+                'friends_s'+str(i)+'_features_visual.h5'))
+        for i in movie10_movies:
+            stimuli_list.append(os.path.join(base_dir, 'movie10', args.modality,
+                'movie10_'+i+'_features_visual.h5'))
+            
+    elif args.modality == 'language_multilingual':
+        for i in friends_seasons:
+            stimuli_list.append(os.path.join(base_dir, 'friends', args.modality,
+                'friends_s'+str(i)+'_features_language.h5'))
+        for i in movie10_movies:
+            stimuli_list.append(os.path.join(base_dir, 'movie10', args.modality,
+                'movie10_'+i+'_features_language.h5'))
         
-            if args.modality == 'language_multi' or args.modality == 'visual' or args.modality == 'audio' or args.modality == 'emotion':
-                #chunks_per_movie.append(len(data[movie][args.modality]))
-                chunks_per_movie.append(len(data[movie]['language']))
-            else:
-                print('error')
-            movie_splits.append(movie)
-        del data
+    elif args.modality == 'audio_emo':
+        stimuli_list1 = []
+        stimuli_list2 = []
+        for i in friends_seasons:
+            stimuli_list.append(os.path.join(base_dir, 'friends', 'audio_low',
+                'friends_s'+str(i)+'_features_audio_low_level.h5'))
+            stimuli_list2.append(os.path.join(base_dir, 'friends', args.modality,
+                'friends_s'+str(i)+'_emo_features_audio.h5'))
+        for i in movie10_movies:
+            stimuli_list.append(os.path.join(base_dir, 'movie10', 'audio_low',
+                'movie10_'+i+'_features_audio_low_level.h5'))
+            stimuli_list2.append(os.path.join(base_dir, 'movie10', args.modality,
+                'movie10_'+i+'_emo_features_audio.h5'))
 
-    # Convert NaN values to zeros (PCA doesn't accept NaN values)
-    print(features.shape)
+    if args.modality != 'audio_emo':
+    # Load the stimulus features for the encoding train stimuli
+        movie_splits = []
+        chunks_per_movie = []
+        for i, stim_dir in tqdm(enumerate(stimuli_list)):
+            data = h5py.File(stim_dir, 'r')
+            for m, movie in enumerate(data.keys()):
+            
+                if i == 0 and m == 0: # if first episode of first season
+                    if args.modality in ['audio', 'language', 'visual']:
+                        features = np.asarray(data[movie][args.modality])
+                    elif args.modality == 'language_multilingual':
+                        features = np.asarray(data[movie]['language'])
+                    elif args.modality == 'visual_videomae':
+                        raw = np.asarray(data[movie]['visual']
+                        reshaped = raw.reshape(-1, 2048, 1408)  # (n_samples, 2048, 1408)
+                        # Pooling
+                        avg_pool = reshaped.mean(axis=1)                           
+                        max_pool = reshaped.max(axis=1)                            
+                        pooled = np.concatenate([avg_pool, max_pool], axis=1) 
+                        features = pooled 
+                else:
+                    if args.modality in ['audio', 'language', 'visual']:
+                        features = np.append(
+                            features, np.asarray(data[movie][args.modality]), 0)      
+                    elif args.modality == 'language_multilingual':
+                        features = np.append(
+                            features, np.asarray(data[movie]['language']), 0)
+                    elif args.modality == 'visual_videomae':
+                        raw = np.asarray(data[movie]['visual']
+                        reshaped = raw.reshape(-1, 2048, 1408)  # (n_samples, 2048, 1408)
+                        # Pooling
+                        avg_pool = reshaped.mean(axis=1)                           
+                        max_pool = reshaped.max(axis=1)                            
+                        pooled = np.concatenate([avg_pool, max_pool], axis=1) 
+                        features = np.append(features, pooled, axis=0)
+        
+                if args.modality in ['audio', 'language', 'visual']:
+                    chunks_per_movie.append(len(data[movie][args.modality]))
+                elif args.modality == 'language_multilingual':
+                    chunks_per_movie.append(len(data[movie]['language']))
+                elif args.modality == 'visual_videomae':
+                    chunks_per_movie.append(len(data[movie]['visual']))
+                
+                movie_splits.append(movie)
+            del data
+
+    else: ### audio_emo
+        
+################## insert here #########################
+    
+    
     if args.modality == 'visual':
         features = features.reshape(features.shape[0], -1)
-    #features = np.concatenate([features[:, :1024], features[:, -1024:]], axis=1)
+    elif args.modality == 'audio_emo':
+        features2 = features2.squeeze(1)
+        features = np.concatenate([features1, features2], axis=1)
     print(features.shape)
     features = np.nan_to_num(features)
 
-    # z-score the features
+    #z-score the features
     scaler = StandardScaler()
     scaler.fit(features)
     features = scaler.transform(features)
@@ -150,36 +202,71 @@ if args.test == 1:
     chunks_per_movie = []
 
     for season in tqdm(test_seasons):
-        data_dir = os.path.join(base_dir, 'friends', args.modality,
-            #'friends_s'+str(season)+'_features_'+args.modality+'.h5')
-            'friends_s'+str(season)+'_features_language.h5')
-        data = h5py.File(data_dir, 'r')
-        for m, movie in enumerate(data.keys()):
-            if season == test_seasons[0] and m == 0: # if first episode of first season
-                if args.modality == 'language_multi' or args.modality == 'visual' or args.modality == 'audio' or args.modality == 'emotion':
-                    #features = np.asarray(data[movie][args.modality])
-                    features = np.asarray(data[movie]['language'])
+        if args.modality in ['audio', 'language', 'visual']:
+            data_dir = os.path.join(base_dir, 'friends', args.modality,
+                'friends_s'+str(season)+'_features_'+args.modality+'.h5')
+        elif args.modality == 'language_multilingual':
+            data_dir = os.path.join(base_dir, 'friends', args.modality,
+                'friends_s'+str(season)+'_features_language.h5')
+        elif args.modality == 'visual_videomae':
+            data_dir = os.path.join(base_dir, 'friends', args.modality,
+                'friends_s'+str(season)+'_features_visual.h5')
+        elif args.modality == 'audio_emo':
+            data_dir1 = os.path.join(base_dir, 'friends', 'audio_low',
+                'friends_s'+str(season)+'_features_audio_low_level.h5')
+            data_dir2 = os.path.join(base_dir, 'friends', args.modality,
+                'friends_s'+str(season)+'emo_features_audio.h5')
+    
+        if args.modality != 'audio_emo':
+            data = h5py.File(data_dir, 'r')
+            for m, movie in enumerate(data.keys()):
+                if season == test_seasons[0] and m == 0: # if first episode of first season
+                
+                    if args.modality in ['audio', 'visual', 'language']:
+                        features = np.asarray(data[movie][args.modality])
+                    elif args.modality == 'language_multilingual':
+                        features = np.asarray(data[movie]['language'])
+                    elif args.modality == 'visual_videomae':
+                        raw = np.asarray(data[movie]['visual']
+                        reshaped = raw.reshape(-1, 2048, 1408)  # (n_samples, 2048, 1408)
+                    # Pooling
+                        avg_pool = reshaped.mean(axis=1)                           
+                        max_pool = reshaped.max(axis=1)                            
+                        pooled = np.concatenate([avg_pool, max_pool], axis=1)
+                        features = pooled
                 else:
-                    print('error')
-            else:
-                if args.modality == 'language_multi' or args.modality == 'visual' or args.modality == 'audio' or args.modality == 'emotion':
-                    features = np.append(
-                        #features, np.asarray(data[movie][args.modality]), 0)
-                        features, np.asarray(data[movie]['language']), 0)
-                else:
-                    print('error')
-            if args.modality == 'language_multi' or args.modality == 'visual' or args.modality == 'audio' or args.modality == 'emotion':
-                #chunks_per_movie.append(len(data[movie][args.modality]))
-                chunks_per_movie.append(len(data[movie]['language']))
-            else:
-                print('error')
-            movie_splits.append(movie)
-        del data
+                    if args.modality in ['audio', 'visual', 'language']:
+                        features = np.append(features, np.asarray(data[movie][args.modality]), 0)
+                    elif args.modality == 'language_multilingual':
+                        features = np.append(features, np.asarray(data[movie]['language']), 0)
+                    elif args.modality == 'visual_videomae':
+                        raw = np.asarray(data[movie]['visual']
+                        reshaped = raw.reshape(-1, 2048, 1408)  # (n_samples, 2048, 1408)
+                        # Pooling
+                        avg_pool = reshaped.mean(axis=1)                           
+                        max_pool = reshaped.max(axis=1)                            
+                        pooled = np.concatenate([avg_pool, max_pool], axis=1)
+                        features = np.append(features, pooled, axis=0)
+                    
+                if args.modality in ['audio', 'visual', 'language']:
+                    chunks_per_movie.append(len(data[movie][args.modality]))
+                elif args.modality == 'language_multilingual':
+                    chunks_per_movie.append(len(data[movie]['language']))
+                elif args.modality == 'visual_videomae':
+                    chunks_per_movie.append(len(data[movie]['visual']))
 
-    print(features.shape)
+                movie_splits.append(movie)
+            del data
+
+        else: #### audio emo
+    ###############i nsert code here #####################
+
     if args.modality == 'visual':
         features = features.reshape(features.shape[0], -1)
-    #features = np.concatenate([features[:, :1024], features[:, -1024:]], axis=1)
+    if args.modality == 'audio_emo':
+        features2 = features2.squeeze(1)
+        features = np.concatenate([features1, features2], axis=1)
+    
     print(features.shape)
     # Convert NaN values to zeros (PCA doesn't accept NaN values)
     features = np.nan_to_num(features)
